42d6de5bdf0ef3883a236b77018f005a5cea9278
takumiiimo <63715509+takumiiimo@users.noreply.github.com>
2021-06-09 21:24:05 +0900

    デフォルト値

--- src/index.js
+++ src/index.js
@@ -89,23 +89,16 @@
  * 分割代入
  */
 
-const myProfile = {
-  name: "akiba",
-  age: 27
+/**
+ * デフォルト値、引数など
+ */
+const sayHello = (name) => {
+  if (name === "takumi") {
+    return console.log(`こんにちは!${name}さん!`);
+  } else {
+    return console.log("error");
+  }
 };
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-console.log(message1);
-
-const { name, age } = myProfile;
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
-
-// 配列
-const myProfile2 = ["takumi", 27];
-
-const message3 = `名前は${myProfile2[0]}です。年齢は${myProfile2[1]}歳です。`;
-console.log(message3);
-
-const [name2, age2] = myProfile2;
-const message4 = `名前は ${name2}です。年齢は${age2}歳です。`;
-console.log(message4);
+sayHello("akiba");
+const sayHello1 = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello1("");
