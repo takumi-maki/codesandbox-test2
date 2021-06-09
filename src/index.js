5716fbfdfa6edc8d756091a336952f2753a1529b
takumiiimo <63715509+takumiiimo@users.noreply.github.com>
2021-06-09 21:10:40 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -66,21 +66,46 @@
  * アロー関数
  */
 // 従来の関数
-function func1(str) {
-  return str;
-}
-console.log(func1("func1です"));
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("func1です"));
 
-// アロー関数
-const func2 = (s) => {
-  return s;
+// // アロー関数
+// const func2 = (s) => {
+//   return s;
+// };
+// console.log(func2("func2です"));
+
+// // 処理が1行の場合returnは省略できる
+// const func4 = (str) => str;
+// console.log(func4("func4です"));
+
+// // 2つの引数を計算
+// const func5 = (num1, num2) => num1 + num2;
+// console.log(func5(10, 20));
+
+/**
+ * 分割代入
+ */
+
+const myProfile = {
+  name: "akiba",
+  age: 27
 };
-console.log(func2("func2です"));
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+console.log(message1);
 
-// 処理が1行の場合returnは省略できる
-const func4 = (str) => str;
-console.log(func4("func4です"));
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
 
-// 2つの引数を計算
-const func5 = (num1, num2) => num1 + num2;
-console.log(func5(10, 20));
+// 配列
+const myProfile2 = ["takumi", 27];
+
+const message3 = `名前は${myProfile2[0]}です。年齢は${myProfile2[1]}歳です。`;
+console.log(message3);
+
+const [name2, age2] = myProfile2;
+const message4 = `名前は ${name2}です。年齢は${age2}歳です。`;
+console.log(message4);
