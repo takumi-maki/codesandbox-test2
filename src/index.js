3c0ea49e8661e9db823c30a79f71f521e8faadd5
takumiiimo <63715509+takumiiimo@users.noreply.github.com>
2021-06-09 14:47:45 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -41,7 +41,23 @@
 // val4.address = "Nagano";
 // console.log(val4);
 
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "卓見";
+const age = 27;
+// 「私の名前はtakumiです。年齢は27歳です。」
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
